d37caff25ba1e92f4c0d6d84367dda2d35499461
Marius <marius@example.com>
2022-11-25 09:41:23 +0200

    sizeHandler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css';
 import Title from './components/Title';
+import { useState } from 'react';
 
 function App() {
   console.log('App.jsx created');
+
+  const [h1Size, setH1Size] = useState('1rem');
+  // const h1Size = '1rem'
+  function sizeHandler() {
+    console.log('size to 3rem');
+    setH1Size('3rem');
+  }
+
+  // paspaudus an h1 el, padidini jo dydi iki 2rem
   return (
     <div className='App'>
-      <h1>Lifecycle</h1>
+      <h1 onClick={sizeHandler} style={{ fontSize: h1Size }}>
+        Lifecycle
+      </h1>
       <Title>Events and Hooks</Title>
     </div>
   );
