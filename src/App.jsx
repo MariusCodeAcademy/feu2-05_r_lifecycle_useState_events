058d430f100b628e67dec42cda31e78d0ad1d542
Marius <marius@example.com>
2022-11-25 12:18:06 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className='App container'>
       <MainTitle />
       <Counter />
-      <Counter>Push ups</Counter>
+
       {/* <Title>Events and Hooks</Title> */}
       {/* <Users /> */}
     </div>
