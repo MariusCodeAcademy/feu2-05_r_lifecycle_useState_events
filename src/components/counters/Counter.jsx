00209df93768bdcebb08d39e4eb21320ec901db8
Marius <marius@example.com>
2022-11-25 10:54:58 +0200

    incrementHandler

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -1,12 +1,23 @@
 import './counter.css';
+import { useState } from 'react';
 
 function Counter() {
+  // 1. state counterValue, useState
+  const [counterValue, setCounterValue] = useState(0);
+
+  // const counterValue = stateArr[0];
+  // const setCounterValue = stateArr[1];
+  // 2. increaseHandler, decreaseHandler fn kurios didins ir mazins skaitliuka
+  function incrementHandler() {
+    setCounterValue((prevCounterValue) => prevCounterValue + 1);
+  }
+
   return (
     <div className='counter card'>
       <h3>Counter</h3>
-      <h2 className='counter__value'>0</h2>
+      <h2 className='counter__value'>{counterValue}</h2>
       <div className='control'>
-        <button>+</button>
+        <button onClick={incrementHandler}>+</button>
         <button>-</button>
       </div>
     </div>
