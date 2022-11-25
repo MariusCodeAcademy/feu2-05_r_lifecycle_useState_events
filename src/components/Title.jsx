b91693f37babfb18309c08558753bddce8122df6
Marius <marius@example.com>
2022-11-25 08:57:42 +0200

    setToShowState

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 function Title(props) {
   console.log('Title.jsx created');
 
+  let toShow = true;
+  // toShowState vietinis kintamasis kuri galim atvaizduot ir jo reiksme pakeiciam su setToShowState(nauja reiksme)
+  // !!!! niekada neprilyginti toShowState kazkam su '='
+  const [toShowState, setToShowState] = useState(true);
+  // panaudojau setToShowState fn kad pakeisti toShowState reiksme i false
+  // setToShowState(false);
+
   function titleHideHandler() {
     console.log('titleHideHandler');
+    setToShowState(false);
   }
 
   return (
     <div>
-      <h2>{props.children}</h2>
+      <h3>toShowState reiksme: {toShowState.toString()}</h3>
+      {toShowState && <h2>{props.children}</h2>}
       <button onClick={titleHideHandler}>hide title</button>
     </div>
   );
