d9eedae5f99b562c0fa880de2a82e9c1e45238a4
Marius <marius@example.com>
2022-11-25 10:27:23 +0200

    MainTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
 import './App.css';
 import Title from './components/Title';
-import { useState } from 'react';
+import MainTitle from './components/MainTitle';
 
 function App() {
   console.log('App.jsx created');
 
-  const [h1Size, setH1Size] = useState('1rem');
-  // const h1Size = '1rem'
-  function sizeHandler() {
-    console.log('size to 3rem');
-    setH1Size('3rem');
-  }
-
-  function increaseTitleSizeHandler() {
-    setH1Size((prevH1Size) => {
-      // pasiimti dabartine reiksme
-      console.log('prevH1Size ===', prevH1Size);
-      // issitraukti skaitine dali
-      let skDalisBeRem = parseFloat(prevH1Size);
-      console.log('skDalisBeRem ===', skDalisBeRem);
-      // ja padidinti
-      skDalisBeRem++;
-      // prideti atgal 'rem'
-      const padidintaSuRem = skDalisBeRem + 'rem';
-      // setitini state su reiksme
-      return `${skDalisBeRem}rem`;
-    });
-  }
-
-  function resetSize() {
-    setH1Size('2rem');
-  }
-
   // paspaudus an h1 el, padidini jo dydi iki 2rem
   return (
     <div className='App'>
-      {/* grazinti h1 dydi i 2rem */}
-      <button onClick={resetSize}>reset</button>
-      <h1 onClick={increaseTitleSizeHandler} style={{ fontSize: h1Size }}>
-        Lifecycle
-      </h1>
-
+      <MainTitle />
       <Title>Events and Hooks</Title>
     </div>
   );
