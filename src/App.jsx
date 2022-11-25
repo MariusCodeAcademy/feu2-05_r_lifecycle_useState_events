7b7959029b25288198d996c4fd0e4d4eec3ebcb1
Marius <marius@example.com>
2022-11-25 11:32:48 +0200

    empty users

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Title from './components/Title';
 import MainTitle from './components/MainTitle';
 import Counter from './components/counters/Counter';
+import Users from './components/Users';
 
 function App() {
   console.log('App.jsx created');
@@ -10,8 +11,9 @@ function App() {
   return (
     <div className='App container'>
       <MainTitle />
-      <Counter />
+      {/* <Counter /> */}
       {/* <Title>Events and Hooks</Title> */}
+      <Users />
     </div>
   );
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,9 @@
+function Users() {
+  return (
+    <div>
+      <h2>Users from db</h2>
+    </div>
+  );
+}
+
+export default Users;
