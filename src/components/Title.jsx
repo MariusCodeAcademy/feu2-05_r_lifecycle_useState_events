9029be12c45e5d34eeb6ed33fa2009dc9cb2f0cd
Marius <marius@example.com>
2022-11-25 09:30:19 +0200

    prevToShowState

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -17,12 +17,18 @@ function Title(props) {
 
   // paspaudus mygtuka keisti toShowState is true i false ir atvirksciai
   // apsirasyti toggleTitleHandler
+  function toggleTitleHandler() {
+    // keiciam buse i priesinga dabartinei
+    // kai musu keiciamas state, priklauso nuo buvusio state, tai mes turim atnaujinti argumente su funkcija
+    // setToShowState(!toShowState); // veiks paprastom applinkybem
+    setToShowState((prevToShowState) => !prevToShowState); // kai state priklauso nuo pries tai buvusio
+  }
 
   return (
     <div>
       <h3>toShowState reiksme: {toShowState.toString()}</h3>
       {toShowState && <h2>{props.children}</h2>}
-      <button onClick={titleHideHandler}>hide title</button>
+      <button onClick={toggleTitleHandler}>{toShowState ? 'hide' : 'show'} title</button>
     </div>
   );
 }
